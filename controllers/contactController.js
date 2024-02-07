4071ef43b22a17ed54fae7b23b7ec844378eee3d
ASAD2723 <khanasad323@gmail.com>
2024-02-07 18:01:35 +0530

    final closing this now

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,24 +1,41 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModels");
 
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get all contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 const createContact = asyncHandler(async (req, res) => {
   console.log("The reques body is :", req.body);
-  const { name, email, ohone } = req.body;
+  const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are mandotary");
   }
-  res.status(201).json({ message: "Create Contacts" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
 });
 
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get Contacts for ${req.params.id}` });
+  const contact = await Contact.findbyId(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
 });
 
 const updateContact = asyncHandler(async (req, res) => {
+  //   const contact = await Contact.findbyId(req.params.id);
+  //   if (!contact) {
+  //     res.status(404);
+  //     throw new Error("Contact not found");
+  //   }
   res.status(200).json({ message: `Update contacts for ${req.params.id}` });
 });
 
